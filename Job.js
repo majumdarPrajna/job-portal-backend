159bc51bbfaba2197de2109adabe0801c8be6fda
Prajna Majumdar <100031258+majumdarPrajna@users.noreply.github.com>
2025-08-01 10:46:22 +0530

    Job.js

---
+++ Job.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  company: String,
+  location: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Job', jobSchema);
